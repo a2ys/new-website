f8532ea2b6ea49dba392d89fa3221391fc97aab8
Aayush Shukla <oreus.aks@gmail.com>
2024-06-10 22:34:12 +0530

    Created the works page and added two projects

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -1,5 +1,77 @@
 const Works = () => {
-  return <div>Works</div>;
+  return (
+    <div className="pt-10 mt-40 mb-40 lg:pt-30 lg:mt-30 lg:mb-40 sup:mb-40">
+      <div
+        data-animate="fade"
+        className="mod-container in-viewport"
+        data-in-viewport="i178"
+      >
+        <p className="mb-3 text-base font-semibold tracking-wide uppercase md:text-lg text-blue-600 lg:mb-5">
+          Showcase
+        </p>{" "}
+        <h1 className="w-full text-black h1 lg:max-w-4/5 sup:max-w-4/6">
+          Check out my recent projects.{" "}
+        </h1>
+      </div>
+      <div className="mod-container mt-40">
+        <div className="flex flex-wrap justify-between row">
+          <a
+            href="/works/chess-ai"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block w-full column lg:w-6/12 xl:w-5/12 mb-15 lg:mb-0 in-viewport below-viewport"
+          >
+            <div className="relative mb-10 thumbnail">
+              <figure className="relative z-10 overflow-hidden">
+                <div className="absolute inset-0 z-10 bg-black overlay"></div>{" "}
+                <img
+                  src="/assets/chess.jpg"
+                  width="640"
+                  height="597"
+                  alt="Thumbnail for Chess AI"
+                  loading="lazy"
+                  className="w-full h-auto"
+                />
+              </figure>{" "}
+              <div className="z-0 border-line"></div>
+            </div>{" "}
+            <h2 className="mb-2 h2">Chess AI in Python</h2>{" "}
+            <p>
+              With plain Python, I created a chess engine and an AI which can
+              play chess using the engine.
+            </p>{" "}
+            <p className="mt-4 text-xs font-semibold tracking-wider uppercase text-blue-600">
+              Read More
+            </p>
+          </a>
+          <a
+            href="/works/portfolio"
+            className="block w-full column lg:w-6/12 xl:w-5/12 mb-15 lg:mb-0 lg:mt-30 in-viewport below-viewport"
+          >
+            <div className="relative mb-10 thumbnail">
+              <figure className="relative z-10 overflow-hidden">
+                <div className="absolute inset-0 z-10 bg-black overlay"></div>{" "}
+                <img
+                  src="/assets/portfolio.png"
+                  width="640"
+                  height="597"
+                  alt="Thumbnail for Portfolio Website"
+                  loading="lazy"
+                  className="w-full h-auto"
+                />
+              </figure>{" "}
+              <div className="z-0 border-line"></div>
+            </div>{" "}
+            <h2 className="mb-2 h2">Portfolio Website</h2>{" "}
+            <p>My portfolio website created with React.</p>{" "}
+            <p className="mt-4 text-xs font-semibold tracking-wider uppercase text-blue-600">
+              Read More
+            </p>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Works;
