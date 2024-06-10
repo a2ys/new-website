e131ee3f0e2bcc90cd524406e6d3cd84e1d8ad7c
Aayush Shukla <oreus.aks@gmail.com>
2024-06-10 18:31:27 +0530

    Changed the base class name and tweaked the Footer a bit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,12 @@
 const Footer = () => {
   return (
     <footer className="py-10 mt-20 text-white bg-black lg:mt-40 lg:py-20">
-      <div className="container">
+      <div className="mod-container">
         <div className="flex flex-wrap justify-between items-center">
           <div className="flex flex-col lg:w-4/6">
             <div>
               <h4 className="mb-2 text-sm font-semibold tracking-wider uppercase text-blue-300 lg:mb-4">
-                Interested in working together?
+                Interested in working together? Drop a mail.
               </h4>{" "}
               <a
                 href="mailto:me@a2ys.dev"
