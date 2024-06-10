b5c821acc1e54a91525b8b99a5823273a8fdc6f7
Aayush Shukla <oreus.aks@gmail.com>
2024-06-10 16:19:22 +0530

    Creating an About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,22 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <main>
+      <div className="pt-10 mt-20 mb-20 lg:pt-30 lg:mt-30 lg:mb-40 sup:mb-40">
+        <div
+          data-animate="fade"
+          className="container in-viewport"
+          data-in-viewport="i178"
+        >
+          <p className="mb-3 text-base font-semibold tracking-wide uppercase md:text-lg text-blue-600 lg:mb-5">
+            About Me
+          </p>{" "}
+          <h1 className="w-full text-black h1 lg:max-w-4/5 sup:max-w-4/6">
+            Background, Skills and Future.{" "}
+          </h1>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default About;
