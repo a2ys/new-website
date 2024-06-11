ab844dc9e38312537fc1fb1f33ad7f0345e6b2a9
Aayush Shukla <oreus.aks@gmail.com>
2024-06-11 23:36:28 +0530

    Moved all the social links to Footer, going to ditch the menu completely

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -36,12 +36,57 @@ const Footer = () => {
                   title="Link to LinkedIn page"
                   target="_blank"
                   rel="me nofollow noreferrer"
-                  className="inline-block text-white"
+                  className="inline-block pr-4 text-white"
                 >
                   <svg className="feather">
                     <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
                     <rect x="2" y="9" width="4" height="12"></rect>
                     <circle cx="4" cy="4" r="2"></circle>
+                  </svg>
+                </a>
+                <a
+                  href="https://twitter.com/unreal_sapien"
+                  title="Link to Twitter"
+                  target="_blank"
+                  rel="me nofollow noreferrer"
+                  className="inline-block pr-4 text-white"
+                >
+                  <svg className="feather">
+                    <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+                  </svg>
+                </a>
+                <a
+                  href="https://blog.a2ys.dev"
+                  title="Link to Blog"
+                  target="_blank"
+                  rel="me nofollow noreferrer"
+                  className="inline-block pr-4 text-white"
+                >
+                  <svg className="feather">
+                    <path d="M12 19l7-7 3 3-7 7-3-3z"></path>
+                    <path d="M18 13l-1.5-7.5L2 2l3.5 14.5L13 18l5-5z"></path>
+                    <path d="M2 2l7.586 7.586"></path>
+                    <circle cx="11" cy="11" r="2"></circle>
+                  </svg>
+                </a>
+                <a
+                  href="https://instagram.com/_cococolastic_"
+                  title="Link to Instagram"
+                  target="_blank"
+                  rel="me nofollow noreferrer"
+                  className="inline-block pr-4 text-white"
+                >
+                  <svg className="feather">
+                    <rect
+                      x="2"
+                      y="2"
+                      width="20"
+                      height="20"
+                      rx="5"
+                      ry="5"
+                    ></rect>
+                    <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+                    <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
                   </svg>
                 </a>
               </nav>
@@ -87,12 +132,50 @@ const Footer = () => {
                 title="Link to LinkedIn page"
                 target="_blank"
                 rel="me nofollow noreferrer"
-                className="inline-block text-white"
+                className="inline-block pr-4 text-white"
               >
                 <svg className="feather">
                   <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
                   <rect x="2" y="9" width="4" height="12"></rect>
                   <circle cx="4" cy="4" r="2"></circle>
+                </svg>
+              </a>
+              <a
+                href="https://twitter.com/unreal_sapien"
+                title="Link to Twitter"
+                target="_blank"
+                rel="me nofollow noreferrer"
+                className="inline-block pr-4 text-white"
+              >
+                <svg className="feather">
+                  <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+                </svg>
+              </a>
+              <a
+                href="https://blog.a2ys.dev"
+                title="Link to Blog"
+                target="_blank"
+                rel="me nofollow noreferrer"
+                className="inline-block pr-4 text-white"
+              >
+                <svg className="feather">
+                  <path d="M12 19l7-7 3 3-7 7-3-3z"></path>
+                  <path d="M18 13l-1.5-7.5L2 2l3.5 14.5L13 18l5-5z"></path>
+                  <path d="M2 2l7.586 7.586"></path>
+                  <circle cx="11" cy="11" r="2"></circle>
+                </svg>
+              </a>
+              <a
+                href="https://instagram.com/_cococolastic_"
+                title="Link to Instagram"
+                target="_blank"
+                rel="me nofollow noreferrer"
+                className="inline-block pr-4 text-white"
+              >
+                <svg className="feather">
+                  <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+                  <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+                  <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
                 </svg>
               </a>
             </nav>
