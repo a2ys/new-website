cd4345488fcbc01212fabdb8e00d8cfa5e32b1e6
Aayush Shukla <oreus.aks@gmail.com>
2024-06-12 11:33:13 +0530

    Created a new navigation style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
-interface HeaderProps {
-  toggleSidebar: () => void;
-}
+import { useState } from "react";
 
-const Header = ({ toggleSidebar }: HeaderProps) => {
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="fixed top-0 left-0 right-0 z-40 flex justify-between px-6 py-4 bg-white lg:bg-transparent">
       <a
@@ -15,23 +19,47 @@ const Header = ({ toggleSidebar }: HeaderProps) => {
       <div>
         <a
           href="/work"
-          className="text-sm font-bold pr-4 tracking-wider text-black uppercase"
+          className="header-link hidden min-[1024px]:inline-block text-l font-bold pr-4 tracking-wider text-black uppercase"
         >
           Work
         </a>
         <a
           href="/about"
-          className="text-sm font-bold pr-4 tracking-wider text-black uppercase"
+          className="header-link hidden min-[1024px]:inline-block text-l font-bold tracking-wider text-black uppercase"
         >
           About
         </a>
-        <button
-          type="button"
-          className="text-sm font-medium tracking-wider text-black uppercase"
-          onClick={toggleSidebar}
-        >
-          Menu
-        </button>
+        <div className="relative flex items-center justify-center min-[1024px]:hidden">
+          <button onClick={handleToggle}>
+            {isMenuOpen ? (
+              <svg className="feather">
+                <line x1="18" y1="6" x2="6" y2="18"></line>
+                <line x1="6" y1="6" x2="18" y2="18"></line>
+              </svg>
+            ) : (
+              <svg className="feather">
+                <line x1="3" y1="12" x2="21" y2="12"></line>
+                <line x1="3" y1="6" x2="21" y2="6"></line>
+                <line x1="3" y1="18" x2="21" y2="18"></line>
+              </svg>
+            )}
+          </button>
+        </div>
+        {isMenuOpen && (
+          <div className="absolute top-full right-8 z-10 flex w-auto flex-col justify-center rounded-xl bg-black min-[1024px]:hidden">
+            <div className="space-y-4 px-10 py-10 min-[1024px]:hidden">
+              <a href="/" className="block text-xl font-medium text-white">
+                Home
+              </a>
+              <a href="/work" className="block text-xl font-medium text-white">
+                Work
+              </a>{" "}
+              <a href="/about" className="block text-xl font-medium text-white">
+                About
+              </a>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
