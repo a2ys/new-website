7e83ee1d00488fdedd5683ad4201de7435b1a064
Aayush Shukla <oreus.aks@gmail.com>
2024-06-12 11:32:13 +0530

    Completely removed the sidebar, was redundant for just 2 links

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import "./index.css";
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Work from "./pages/Work";
 import About from "./pages/About";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Sidebar from "./components/Sidebar";
 import NotFound from "./pages/NotFound";
 
 function App() {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
-
   return (
     <Router>
       <div className="inner">
-        <Header toggleSidebar={toggleSidebar} />
-        <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/work" element={<Work />} />
